94fa3c3a5169f181fd78e37e20afb9e7384071ae
dwyfrequency <jdwy215@gmail.com>
2018-07-27 00:15:02 -0400

    updated left right example

--- functional-examples/examples/03-right-left.js
+++ functional-examples/examples/03-right-left.js
@@ -1,66 +1,92 @@
-const Right = x => ({
+const log = console.log;
+
+class Right {
+  constructor(x) {
+    this.x = x;
+  }
+
   // Right applies f to x
-  map: f => Right(f(x)),
+  map(f) {
+    return Right.of(f(this.x));
+  }
 
   // applies the function on the right and returns raw value
-  fold: (f, g) => g(x),
+  fold(f, g) {
+    return g(this.x);
+  }
 
   // custom getter function -- called by console.log
-  inspect: () => `Right(${x})`
-});
+  inspect() {
+    return `Right(${this.x})`;
+  }
 
-const Left = x => ({
+  // boxes value again
+  static of(x) {
+    return new Right(x);
+  }
+}
+
+class Left {
+  constructor(x) {
+    this.x = x;
+  }
+
   // Left ignores f, simply passes ahead raw value x
-  map: f => Left(x),
+  map(f) {
+    return Left.of(this.x);
+  }
 
   // applies the function on the left and returns raw value
-  fold: (f, g) => f(x),
+  fold(f, g) {
+    return f(this.x);
+  }
 
   // custom getter function -- called by console.log
-  inspect: () => `Left(${x})`
-});
+  inspect() {
+    return `Left(${this.x})`;
+  }
+
+  static of(x) {
+    return new Left(x);
+  }
+}
 
 // this will apply the functions
-const resultRight = Right(3)
+const resultRight = Right.of(3)
   .map(x => x + 1)
   .map(x => x / 2);
-
 // this will ignore all functions and return itself
-const resultLeft = Left(3)
+const resultLeft = Left.of(3)
   .map(x => x + 1)
   .map(x => x / 2);
 
-console.log(`Right(3) transformed: `, resultRight); //=> Right(2)
-console.log(`Left(3) transformed: `, resultLeft); //=> Left(3)
+log(`Right(3) transformed: `, resultRight); //=> Right(2)
+log(`Left(3) transformed: `, resultLeft); //=> Left(3)
 
 // passing Right value,
 // all functions are applied
 // and result is returned
-const result = Right(2)
+const result = Right.of(2)
   .map(x => x + 1)
   .map(x => x / 2)
-
   // left function handles error, right function returns the value
   .fold(x => "error", x => x);
 
-console.log(
-  `Right(2) transformed and unwrapped with (x => 'error', x => x): `,
-  result
+log(
+  `Right(2) transformed and unwrapped with (x => 'error', x => x): ${result}`
 ); //=> 1.5
 
 // passing Left value,
 // so all functions are ignored
 // and error is returned
-const resultError = Left(2)
+const resultError = Left.of(2)
   .map(x => x + 1)
   .map(x => x / 2)
-
   // left function handles error, right function returns the value
   .fold(x => "error", x => x);
 
-console.log(
-  `Left(2) transformed and unwrapped with (x => 'error', x => x): `,
-  resultError
+log(
+  `Left(2) transformed and unwrapped with (x => 'error', x => x): ${resultError}`
 ); //=> error
 
 const findColor = name =>
@@ -73,14 +99,14 @@ const resultColor = findColor("red")
   .slice(1)
   .toUpperCase();
 
-console.log(`findColor('red') transformed is: `, resultColor);
+log(`findColor('red') transformed is: ${resultColor}`);
 
 const findColorCases = name => {
   const found = { red: "#ff4444", blue: "#00ff00" }[name];
-  return found ? Right(found) : Left(null);
+  return found ? Right.of(found) : Left.of(null);
 };
 
-console.log(`findColorCases('gray') is: `, findColorCases("gray")); //=> no color
+log(`findColorCases("gray") is:`, findColorCases("gray")); //=> no color
 
 const badColor = findColorCases("gray")
   // map only applies when passed Right,
@@ -88,13 +114,13 @@ const badColor = findColorCases("gray")
   .map(c => c.slice(1))
   .fold(e => "no color", c => c.toUpperCase());
 
-console.log(
+log(
   `findColorCases('gray') transformed and fold with (e => 'no color', c => c.toUpperCase()) is: `,
   badColor
 ); //=> no color
 
 // ensure null (of any falsey value) will always go to Left
-const fromNullable = x => (x != null ? Right(x) : Left(null));
+const fromNullable = x => (x != null ? Right.of(x) : Left.of(null));
 
 // no need anymore to worry about null in our logic
 // much simpler function, no more cases
@@ -107,7 +133,7 @@ const badColorNew = findColorFromNullable("gray")
   .map(c => c.slice(1))
   .fold(e => "no color", c => c.toUpperCase());
 
-console.log(
+log(
   `findColorFromNullable('gray') transformed and fold with (e => 'no color', c => c.toUpperCase()) is: `,
   badColorNew
 ); //=> no color
